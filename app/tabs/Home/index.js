d38f80f8fc8bb62dc1c563b7fb522fc155bce310
nader dabit <dabit3@gmail.com>
2017-06-18 18:28:58 -0500

    basic tab layout

--- app/tabs/Home/index.js
+++ app/tabs/Home/index.js
@@ -6,20 +6,7 @@ const { width } = Dimensions.get('window');
 
 export default Home = () => (
   <View style={styles.container}>
-    <Image
-      resizeMode='contain'
-      style={styles.logo}
-      source={require('../../assets/images/logo.png')}
-    />
-    <Text style={styles.title}>Welcome to Burger Me</Text>
-    <Text style={styles.subTitle}>Serving the best burgers in the world.</Text>
-    <Text style={styles.number} onPress={
-      () => Linking.openURL('https://github.com/react-native-training')
-        .then(() => console.log('calling'))
-        .catch((err) => console.log('error: ', err))
-    }>
-      PH: 650.555.5309
-    </Text>
+    <Text>Hello from Home</Text>
   </View>
 )
 
@@ -30,22 +17,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  logo: {
-    width: width - 60,
-    maxHeight: 60,
-  },
-  title: {
-    fontFamily: 'AlegreyaSansSC-Light',
-    fontSize: 30,
-  },
-  subTitle: {
-    fontFamily: 'AlegreyaSansSC-Regular',
-    fontSize: 16,
-  },
-  number: {
-    color: '#fe0000',
-    fontFamily: 'AlegreyaSansSC-Regular',
-    fontSize: 12,
-    marginTop: 10
-  }
 })
