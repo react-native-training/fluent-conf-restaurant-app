2a7d05a2c5b1ffd5931fb9c68ab9737e5b5bd2a8
nader dabit <dabit3@gmail.com>
2017-06-18 17:40:06 -0500

    finished stacknavigator

--- app/tabs/Menu/routes/Appetizers.js
+++ app/tabs/Menu/routes/Appetizers.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ScrollView, View, Text, StyleSheet } from 'react-native';
 
-import MenuItem from '../../../components/MenuItem';
-
 export default class Appetizers extends React.Component {
   static navigationOptions = {
     title: 'Appetizers'
@@ -12,18 +10,6 @@ export default class Appetizers extends React.Component {
     return (
       <ScrollView>
         <View style={styles.container}>
-          <MenuItem
-            title='Guacamole'
-            price='$6'
-            description='Prepared at tableside'
-            source={require('../../../assets/images/guacamole.jpg')}
-          />
-          <MenuItem
-            title='Tacos'
-            price='$10'
-            description='Served With Salsa, Hot Tortillas, Rice, And Beans'
-            source={require('../../../assets/images/tacos.jpg')}
-          />
         </View>
       </ScrollView>
     )
